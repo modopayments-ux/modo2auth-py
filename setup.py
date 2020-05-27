31f24e1881e13b8dd9768e212b78d6b2185d7c92
Adam Schnaare <adam.schnaare@modopayments.com>
2020-05-27 16:46:29 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name="modo2auth",
+    version="1.0.1",
+    description="Generate authentication details to communicate with Modo servers",
+    long_description=Path("README.md").read_text(),
+    packages=find_packages(),
+    zip_safe=False)
