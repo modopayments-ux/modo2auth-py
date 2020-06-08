f9c54e2816a415c6a57ae2139fb86ed39b30fb01
Adam Schnaare <adam.schnaare@modopayments.com>
2020-06-08 14:37:43 -0500

    Version set

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name="modo2auth",
-    version="1.0.1",
+    version="1.0.0",
     description="Generate authentication details to communicate with Modo servers",
     long_description=Path("README.md").read_text(),
     packages=find_packages(),
